beed2710cf799ce74198b53c0b8c965229ec1328
peterlee <peterlee@wifi-lee.local>
2019-09-18 20:56:28 +0800

    movie

--- MoviePlayer/ContentView.swift
+++ MoviePlayer/ContentView.swift
@@ -27,7 +27,7 @@ struct ListView:View {
     var body: some View{
         NavigationView{
             List(0..<6){ item in
-                NavigationLink.init(destination: MapLandMarkView()) {
+                NavigationLink.init(destination: PlayerViewContainer().padding()) {
                     ImageView()
                 }
             }.navigationBarTitle(Text("First Blood"))
@@ -35,9 +35,69 @@ struct ListView:View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-//        ContentView()
-        ImageView()
+
+import AVKit
+
+struct PlayerViewContainer:View {
+    
+    var body: some View{
+        PlayerView().padding().onDisappear {
+            PlayerView.Coordinator.share.stopPlay()
+        }.onAppear {
+            PlayerView.Coordinator.share.updateLayer()
+        }
+    }
+}
+
+struct PlayerView:UIViewRepresentable {
+    func makeCoordinator() -> PlayerView.Coordinator {
+        Coordinator.share
+    }
+    
+    
+    typealias UIViewType = UIView
+    func makeUIView(context: UIViewRepresentableContext<PlayerView>) -> UIView {
+        let temp = UIView(frame: .zero)
+        context.coordinator.setupView(view: temp)
+        return temp
+    }
+
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+        context.coordinator.updateLayer()
+    }
+    
+   
+    class Coordinator: NSObject {
+        var avplayer:AVPlayer?
+        var playerLayer:AVPlayerLayer?
+        var playerItem:AVPlayerItem?
+        var view:UIView?
+        static let share = Coordinator()
+        override init() {
+            super.init()
+            playerItem = AVPlayerItem(url: URL(string: "https://meiju4.whetyy.com/20190725/ix0Z695V/index.m3u8")!)
+            avplayer = AVPlayer(playerItem: playerItem!)
+            playerLayer = AVPlayerLayer(player: avplayer!)
+        }
+        
+        func  setupView(view:UIView) -> Void {
+            view.layer.addSublayer((playerLayer)!)
+            playerLayer?.frame = UIScreen.main.bounds
+            avplayer?.play()
+            self.view = view
+        }
+        
+        func stopPlay() -> Void {
+            if avplayer != nil {
+                avplayer?.pause()
+                playerLayer?.removeFromSuperlayer()
+            }
+        }
+        
+        func updateLayer() {
+//            playerLayer?.frame = self.view!.bounds
+            playerLayer?.transform = CATransform3DMakeRotation(CGFloat(Double.pi), 0, 0, 0)
+        }
     }
 }
